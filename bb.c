9320012e62a0283242ca0aa5a11415d36c2b39ed
kosmos <kansk.sasha@live.com>
2020-05-11 00:59:19 +0700

    Chanji

--- bb.c
+++ bb.c
@@ -1,325 +1,360 @@
 #include"bb.h"
 
- int bb_errno=ERR_OK;
+int bb_errno = ERR_OK;
 
-int left_shift (bb* vector, int size_of_shift)
+bb* left_shift (bb* vector, int size_of_shift)
 {
+    if (vector==NULL)
+    {
+        bb_errno = ERR_NULL_INPUT;
+        return NULL;
+    }
+
     //checking if the shift is possible
-    if (size_of_shift >(legth_of_parts-1-vector->last_part)*8+7-vector->last_bit)
+    if (size_of_shift > (legth_of_parts - 1 - vector->last_part) * 8 + 7 - vector->last_bit)
     {
         bb_errno = ERR_BIG_SHIFT;
-        return ERR_BIG_SHIFT;
+        return NULL;
     }
- 
-    //checking if the offset is less than one
-    if (size_of_shift <1)
-    {
-        bb_errno = ERR_SMALL_SIZE;
-        return ERR_SMALL_SIZE;
-    }  
- 
-    //shift
-    for (int j = 0; j < size_of_shift; j++)
-    {
-        //need if add another part during the shift
-        int new_part = vector->last_part;
- 
-        for (int i = vector->last_part; i >= 0; i--)
-        {
-            //separate shift for the first part if not all 8 bits are occupied in it
-            if (i == vector->last_part && vector->last_bit!=7)
-            {
-                vector->parts[i] <<= 1;
-                vector->last_bit++;
-                continue;
-            }
- 
-            //inc parts if all bits are occupied in the first part
-            if (i == vector->last_part)
-            {
-                new_part++;
-                vector->last_bit=0;
-            }
- 
-            //shift the last bit of one part to the next part to the first bit
-            if (vector->parts[i]&(1<<7))
-            {
-                vector->parts[i] <<= 1;
-                vector->parts[i + 1] |= (1 << 0);
-            }
-            else
-            {
-                vector->parts[i] <<= 1;
-            }
-        }
- 
-        //replacement if a new part was added or not
-        vector->last_part = new_part;
-    }
- 
-    return ERR_OK;
-}
- 
-int rigth_shift (bb* vector, int size_of_shift)
-{
-    //checking if the shift is possible
-    if (size_of_shift >= vector->last_part*8+1+vector->last_bit)
-    {
-        bb_errno = ERR_BIG_SHIFT;
-        return ERR_BIG_SHIFT;
-    }
- 
+
     //checking if the offset is less than one
     if (size_of_shift < 1)
     {
-        bb_errno = ERR_SMALL_SIZE;
-        return ERR_SMALL_SIZE;
-    }
- 
-    //shift
-    for (int j = 0; j < size_of_shift; j++)
-    {
-        //need if one part can be removed during the shift
-        int new_part = vector->last_part;
- 
-        //buffer for the first bit of part
-        int buf;
- 
-        for (int i = vector->last_part; i >= 0; i--)
-        {
-            //left shift of the very first part
-            if (i == vector->last_part)
-            {
-                if (vector->parts[i] & (1<<0))
-                {
-                    buf = 1;
-                }
-                else
-                {
-                    buf = 0;
-                }
- 
-                vector->parts[i] >>= 1;
- 
-                if (vector->last_bit==0)
-                {
-                    new_part--;
-                    vector->last_bit = 7;
-                }
-                else
-                {
-                    vector->last_bit--;
-                }
-                continue;
-            }
- 
-            //left shift of parts between the first and last
-            if (i!=0)
-            {
-                //another buffer to store the last bit of the current part
-                int last_bit_of_part;
-                if (vector->parts[i] & (1 << 0))
-                {
-                    last_bit_of_part = 1;
-                }
-                else
-                {
-                    last_bit_of_part = 0;
-                }
- 
-                //left shift of the current part
-                vector->parts[i] >>= 1;
- 
-                //recording a bit from the previous part
-                if (buf)
-                {
-                    vector->parts[i] |= (1 << 7);
-                }
- 
-                //replacing the buffer with the current part bit
-                buf = last_bit_of_part;
-            }
- 
-            //left shift of the last part
-            else
-            {
-                vector->parts[i] >>= 1;
-                if (buf)
-                {
-                    vector->parts[i] |= (1 << 7);
-                }
-            }
-        }
- 
-        //replacement if one part was deleted
-        vector->last_part = new_part;
+        bb_errno = ERR_SMALL_SHIFT;
+        return NULL;
     }
 
-    return ERR_OK;
+    bb* vector1 = calloc (1, sizeof (bb));
+    if (vector1 == NULL)
+    {
+        bb_errno = ERR_MEM_NOT_ALLOC;
+        return NULL;
+    }
+    
+    vector1->last_bit = 0;
+    vector1->last_part = 0;
+    uint8_t last_part_of_bits = 0;
+    uint8_t first_part_of_bits = 0;
+    for (int i = 0; i <= vector->last_part; i++)
+    {
+        last_part_of_bits = vector->parts[i] >> (8 - size_of_shift % 8);
+        vector1->parts[i + size_of_shift / 8] = first_part_of_bits | vector->parts[i] << (size_of_shift % 8);
+        first_part_of_bits = last_part_of_bits;
+    }
+
+    //checks if another byte needs to be created when the last bit is shifted
+    if (vector->last_bit + size_of_shift % 8 > 7)
+    {
+        vector1->last_part++;
+        vector1->parts[size_of_shift / 8 + vector->last_part + 1] |= last_part_of_bits;
+    }
+    
+    //calculating the dimension of the resulting vector after the shift
+    vector1->last_bit += (vector->last_bit + size_of_shift % 8)%8;
+    vector1->last_part += vector->last_part + size_of_shift / 8;
+
+    return vector1;
 }
- 
+
+bb* rigth_shift (bb* vector, int size_of_shift)
+{
+    if (vector == NULL)
+    {
+        bb_errno = ERR_NULL_INPUT;
+        return NULL;
+    }
+
+    //checking if the shift is possible
+    if (size_of_shift >= vector->last_part * 8 + 1 + vector->last_bit)
+    {
+        bb_errno = ERR_BIG_SHIFT;
+        return NULL;
+    }
+
+    //checking if the offset is less than one
+    if (size_of_shift < 1)
+    {
+        bb_errno = ERR_SMALL_SHIFT;
+        return NULL;
+    }
+
+    bb* vector1 = calloc (1, sizeof (bb));
+
+    //checking whether memory is allocated
+    if (vector1 == NULL)
+    {
+        bb_errno = ERR_MEM_NOT_ALLOC;
+        return NULL;
+    }
+
+    vector1->last_bit = 0;
+    vector1->last_part = 0;
+    uint8_t first_part_of_bits = 0;
+
+    //separate right shift of the desired right part
+    vector1->parts[0] = vector->parts[size_of_shift / 8] >> (size_of_shift % 8);
+
+    //right shift of the other parts
+    for (int i = size_of_shift / 8+1; i <= vector->last_part; i++)
+    {
+        first_part_of_bits = vector->parts[i] << (8 - size_of_shift % 8);
+        vector1->parts[i - size_of_shift / 8] = vector->parts[i] >> (size_of_shift % 8);
+        vector1->parts[i - size_of_shift / 8 - 1] |= first_part_of_bits;
+    }
+
+    //calculating the dimension of the resulting vector after the shift
+    vector1->last_part = (vector->last_part*8+vector->last_bit - size_of_shift)/8;
+    vector1->last_bit = (vector->last_part * 8 + vector->last_bit - size_of_shift) % 8;
+    
+    return vector1;
+}
+
 bb* bb_from_string (char* string)
 {
-    bb* vector = malloc (sizeof (bb));
+    if (string == NULL)
+    {
+        bb_errno = ERR_NULL_INPUT;
+        return NULL;
+    }
+
+    bb* vector = calloc (1, sizeof (bb));
+
+    //checking whether memory is allocated
+	if (vector==NULL)
+	{
+        bb_errno = ERR_MEM_NOT_ALLOC;
+        return NULL;
+	}
     vector->last_bit = 0;
     vector->last_part = 0;
- 
-    //to add the first bit without shifting
-    int need_shift = 0;
- 
+
     //index for the string
-    int ind_string = 0;
- 
+    int string_idx = 1;
+
+	//read first bit
+	if (string[0]!='\0')
+	{
+		if (string[0]=='1')
+		{
+			vector->parts[0] |= 1;
+		}
+	}
+
     //reading bits from a string and writing them to a vector
-    while (string[ind_string]!='\0')
+    while (string[string_idx] != '\0')
     {
         //checks if the string contains something other than 1 and 0
-        if (string[ind_string] == '1' || string[ind_string] == '0')
+        if (string[string_idx] == '1' || string[string_idx] == '0')
         {
-            if (need_shift)
-            {
-                if (left_shift (vector, 1))
-                {
-                    return NULL;
-                }
-            }
-            need_shift = 1;
- 
+			vector=left_shift (vector, 1);
+
             //adding a bit to the end
-            if (string[ind_string] == '1')
-            {
-                vector->parts[0] |= (1 << 0);
-            }
-            //don't need to add a zero because when you shift it is added
+			vector->parts[0] |= (string[string_idx]-48);
         }
         //if it contains it we output a message
         else
         {
-            return NULL;
+			bb_errno = ERR_WRONG_CHARS;
+			goto free;
         }
-        ind_string++;
+		string_idx++;
     }
 
     return vector;
+
+free:
+	free (vector);
+	return NULL;
 }
- 
+
 char* bb_to_string (bb* vector)
 {
+    if (vector == NULL)
+    {
+        bb_errno = ERR_NULL_INPUT;
+        return NULL;
+    }
+
     //initializing a string and its index
-    int ind_string = 0;
-    char* string = malloc ((ind_string + 1) * sizeof (char));
- 
+    int string_idx = 0;
+    char* string = calloc (1,(vector->last_part*8+vector->last_bit) * sizeof (char));
+
+    //checking whether memory is allocated
+	if (string==NULL)
+	{
+        bb_errno = ERR_MEM_NOT_ALLOC;
+        return NULL;
+	}
+
     //separate addition of bits from the first part since it can be of different lengths
-    for (int i = vector->last_bit; i >=0; i--)
+    for (int i = vector->last_bit; i >= 0; i--)
     {
-        //checking which bit is 1 or 0
-        if (vector->parts[vector->last_part]&(1<<i))
-        {
-            string[ind_string] = '1';
-        }
-        else
-        {
-            string[ind_string] = '0';
-        }
- 
-        //after writing the bit, re-create the string by +1 element
-        ind_string++;
-        string = (char*)realloc (string, (ind_string + 1) * sizeof (char));
+		if (vector->parts[vector->last_part] & (1 << i))
+		{
+			string[string_idx] = '1';
+		}
+		else
+		{
+			string[string_idx] = '0';
+		}
+		string_idx++;
     }
- 
-    //adding bits from other parts of the
-    for (int i = vector->last_part-1; i >=0; i--)
+
+    //adding bits from other parts
+    for (int i = vector->last_part - 1; i >= 0; i--)
     {
-        for (int j = 7; j >=0; j--)
+        for (int j = 7; j >= 0; j--)
         {
-            //checking which bit is 1 or 0
-            if (vector->parts[i] & (1 << j))
-            {
-                string[ind_string] = '1';
-            }
-            else
-            {
-                string[ind_string] = '0';
-            }
- 
-            //after writing the bit, re-create the string by +1 element
-            ind_string++;
-            string = (char*)realloc (string, (ind_string + 1) * sizeof (char));
+			if (vector->parts[i] & (1 << j))
+			{
+				string[string_idx] = '1';
+			}
+			else
+			{
+				string[string_idx] = '0';
+			}
+			string_idx++;
         }
     }
- 
+
     //adding a zero to the end of a line
-    string[ind_string] = '\0';
+    string[string_idx] = '\0';
 
     return string;
 }
- 
+
 bb* bb_disjunction (bb* vector1, bb* vector2)
 {
-    //Checking that the vectors are the same length
-    if (vector1->last_part!=vector2->last_part || vector1->last_bit!=vector2->last_bit)
+    if (vector1==NULL || vector2==NULL)
     {
+        bb_errno = ERR_NULL_INPUT;
         return NULL;
     }
 
-    bb* vector = malloc (sizeof (bb));
+    //if the length of the second vector is longer than the first one then swap them
+    if (vector2->last_part*8+vector2->last_bit>vector1->last_part * 8 + vector1->last_bit)
+    {
+        return bb_disjunction (vector2, vector1);
+    }
+
+    bb* vector = calloc (1,sizeof (bb));
+
+    //checking whether memory is allocated
+    if (vector==NULL)
+    {
+        bb_errno = ERR_MEM_NOT_ALLOC;
+        return NULL;
+    }
     vector->last_bit = vector1->last_bit;
     vector->last_part = vector1->last_part;
- 
+
     //disjunction
-    for (int i = 0; i <= vector1->last_part; i++)
+    for (int i = 0; i <= vector2->last_part; i++)
     {
         vector->parts[i] = vector1->parts[i] | vector2->parts[i];
     }
-    
+    for (int i = vector2->last_part+1; i <= vector1->last_part; i++)
+    {
+        vector->parts[i] = vector1->parts[i];
+    }
+
     return vector;
 }
- 
+
 bb* bb_conjunction (bb* vector1, bb* vector2)
 {
-    //Checking that the vectors are the same length
-    if (vector1->last_part != vector2->last_part || vector1->last_bit != vector2->last_bit)
+    if (vector1 == NULL || vector2 == NULL)
     {
+        bb_errno = ERR_NULL_INPUT;
         return NULL;
     }
 
-    bb* vector = malloc (sizeof (bb));
+    //if the length of the second vector is longer than the first one then swap them
+    if (vector2->last_part * 8 + vector2->last_bit > vector1->last_part * 8 + vector1->last_bit)
+    {
+        return bb_conjunction (vector2, vector1);
+    }
+
+    bb* vector = calloc (1, sizeof (bb));
+
+    //checking whether memory is allocated
+    if (vector == NULL)
+    {
+        bb_errno = ERR_MEM_NOT_ALLOC;
+        return NULL;
+    }
     vector->last_bit = vector1->last_bit;
     vector->last_part = vector1->last_part;
- 
+
     //conjunction
-    for (int i = 0; i <= vector1->last_part; i++)
+    for (int i = 0; i <= vector2->last_part; i++)
     {
         vector->parts[i] = vector1->parts[i] & vector2->parts[i];
     }
+    for (int i = vector2->last_part + 1; i <= vector1->last_part; i++)
+    {
+        vector->parts[i] = vector1->parts[i];
+    }
 
     return vector;
 }
- 
+
 bb* bb_xor (bb* vector1, bb* vector2)
 {
-    //Checking that the vectors are the same length
-    if (vector1->last_part != vector2->last_part || vector1->last_bit != vector2->last_bit)
+    if (vector1 == NULL || vector2 == NULL)
     {
+        bb_errno = ERR_NULL_INPUT;
         return NULL;
     }
 
-    bb* vector = malloc (sizeof (bb));
+    //if the length of the second vector is longer than the first one then swap them
+    if (vector2->last_part * 8 + vector2->last_bit > vector1->last_part * 8 + vector1->last_bit)
+    {
+        return bb_xor (vector2, vector1);
+    }
+
+    bb* vector = calloc (1, sizeof (bb));
+
+    //checking whether memory is allocated
+    if (vector == NULL)
+    {
+        bb_errno = ERR_MEM_NOT_ALLOC;
+        return NULL;
+    }
+
     vector->last_bit = vector1->last_bit;
     vector->last_part = vector1->last_part;
- 
+
     //xor
-    for (int i = 0; i <= vector1->last_part; i++)
+    for (int i = 0; i <= vector2->last_part; i++)
     {
-        vector->parts[i] =vector1->parts[i] ^ vector2->parts[i];
+        vector->parts[i] = vector1->parts[i] ^ vector2->parts[i];
+    }
+    for (int i = vector2->last_part + 1; i <= vector1->last_part; i++)
+    {
+        vector->parts[i] = vector1->parts[i];
     }
 
     return vector;
 }
- 
+
 bb* bb_inversion (bb* vector1)
 {
-    bb* vector = malloc (sizeof (bb));
+    if (vector1 == NULL)
+    {
+        bb_errno = ERR_NULL_INPUT;
+        return NULL;
+    }
+
+    bb* vector = calloc (1, sizeof (bb));
+
+    //checking whether memory is allocated
+    if (vector == NULL)
+    {
+        bb_errno = ERR_MEM_NOT_ALLOC;
+        return NULL;
+    }
+
     vector->last_bit = vector1->last_bit;
     vector->last_part = vector1->last_part;
 
@@ -330,89 +365,59 @@ bb* bb_inversion (bb* vector1)
 
     return vector;
 }
- 
-int cycle_left_shift (bb* vector, int size_of_shift)
+
+bb* cycle_left_shift (bb* vector, int size_of_shift)
 {
-    //checking if the offset is less than one
+    //checking if the shift is less than one
     if (size_of_shift < 1)
     {
-        bb_errno = ERR_SMALL_SIZE;
-        return ERR_SMALL_SIZE;
-    }
- 
-    //cycle left shift
-    for (int i = 0; i < size_of_shift; i++)
-    {
-        //need to remember the current last_bit and last_part and the first bit
-        int first_left_bit=0;
-        int last_part = vector->last_part;
-        int last_bit = vector->last_bit;
- 
-        //if need it to set the first left bit to 1
-        if (vector->parts[last_part]&(1<<last_bit))
-        {
-            first_left_bit = 1;
-        }
- 
-        //making a left shift
-        if (left_shift(vector,1))
-        {
-            return 1;
-        }
- 
-        //returning the values last_bit and last_part to their places
-        vector->last_bit = last_bit;
-        vector->last_part = last_part;
- 
-        //setting the first left bit to the end
-        if (first_left_bit)
-        {
-            vector->parts[0] |= (1 << 0);
-        }
+        bb_errno = ERR_SMALL_SHIFT;
+        return NULL;
     }
 
-    return ERR_OK;
+    if (vector == NULL)
+    {
+        bb_errno = ERR_NULL_INPUT;
+        return NULL;
+    }
+
+    bb* new_vector1 = left_shift (vector, size_of_shift);
+    bb* new_vector2 = rigth_shift (vector, vector->last_part * 8 + vector->last_bit - size_of_shift);
+    bb* new_vector = bb_disjunction (new_vector1, new_vector2);
+
+    free (new_vector2);
+    free (new_vector1);
+
+    new_vector->last_part = vector->last_part;
+    new_vector->last_bit = vector->last_bit;
+
+    return new_vector;
 }
- 
-int cycle_rigth_shift (bb* vector, int size_of_shift)
+
+bb* cycle_rigth_shift (bb* vector, int size_of_shift)
 {
-    //checking if the offset is less than one
+    //checking if the shift is less than one
     if (size_of_shift < 1)
     {
-        bb_errno = ERR_SMALL_SIZE;
-        return ERR_SMALL_SIZE;
+        bb_errno = ERR_SMALL_SHIFT;
+        return NULL;
     }
- 
-    //cycle left shift
-    for (int i = 0; i < size_of_shift; i++)
-    {
-        //need to remember the current last_bit and last_part and the last_rigth_bit
-        int last_rigth_bit = 0;
-        int last_part = vector->last_part;
-        int last_bit = vector->last_bit;
- 
-        //if need it to set the last_rigth_bit to 1
-        if (vector->parts[0] & (1 << 0))
-        {
-            last_rigth_bit = 1;
-        }
- 
-        //making a rigth shift
-        if (rigth_shift (vector, 1))
-        {
-            return 1;
-        }
- 
-        //returning the values last_bit and last_part to their places
-        vector->last_bit = last_bit;
-        vector->last_part = last_part;
- 
-        //setting the last right bit to the beginning
-        if (last_rigth_bit)
-        {
-            vector->parts[last_part] |= (1 << last_bit);
-        }
-    }  
 
-    return ERR_OK;
+    if (vector == NULL)
+    {
+        bb_errno = ERR_NULL_INPUT;
+        return NULL;
+    }
+
+    bb* new_vector1 = rigth_shift (vector, size_of_shift);
+    bb* new_vector2 = left_shift (vector, vector->last_part * 8 + vector->last_bit - size_of_shift);
+    bb* new_vector = bb_disjunction (new_vector1, new_vector2);
+
+    free (new_vector2);
+    free (new_vector1);
+
+    new_vector->last_part = vector->last_part;
+    new_vector->last_bit = vector->last_bit;
+
+    return new_vector;
 }
--- bb.h
+++ bb.h
@@ -8,8 +8,11 @@
 #define legth_of_parts 20
 
 #define ERR_OK 0
-#define ERR_SMALL_SIZE -1
+#define ERR_SMALL_SHIFT -1
 #define ERR_BIG_SHIFT 1
+#define ERR_MEM_NOT_ALLOC 4
+#define ERR_WRONG_CHARS 2
+#define ERR_NULL_INPUT 3
 
 //bb_errno is set by library functions when error occurs.
 extern int bb_errno;
@@ -20,23 +23,191 @@ struct BigBool
     int last_bit;
     int last_part;
 };
- 
+
 typedef struct BigBool bb;
 
-//shifts
-int left_shift (bb* vector, int size_of_shift); //makes a left shift by size_of_shift and return zero in case of successful execution
-int rigth_shift (bb* vector, int size_of_shift); //makes a rigth shift by size_of_shift and return zero in case of successful execution
-int cycle_left_shift (bb* vector, int size_of_shift); //makes a cycle left shift by size_of_shift and return zero in case of successful execution
-int cycle_rigth_shift (bb* vector, int size_of_shift); //makes a cycle rigth shift by size_of_shift and return zero in case of successful execution
+/* -------------------------------------------------------------------------
+ * Shifts
+ * ------------------------------------------------------------------------- */
 
-//transformations
-bb* bb_from_string (char* string); //makes a big bool vector from a string and return pointer to a big bool vector
-char* bb_to_string (bb* vector); //makes a string from a big bool vector and return pointer to a string
+ /*
+  * Funtion: left_shift
+  * -------------------
+  * Do left shift of bool vector
+  * 
+  *        vector: pointer to a vector
+  * size_of_shift: the number by which the left shift will be made
+  * 
+  * returns: a pointer to a vector in case of success
+  *          NULL in case of an error
+  * 
+  * if an error occurs the bb_errno variable is set to:
+  *     ERR_MEM_NOT_ALLOC — memory allocation error
+  *     ERR_NULL_INPUT    — zero incoming data
+  *     ERR_BIG_SHIFT     — too big a shift, going over the border
+  *     ERR_SMALL_SHIFT   — shift less than 1
+  */
+bb* left_shift (bb* vector, int size_of_shift);
 
-//logical operations
-bb* bb_disjunction (bb* vector1, bb* vector2); //makes a disjunction of two big bool vectors and return pointer to a big bool vector
-bb* bb_conjunction (bb* vector1, bb* vector2); //makes a conjunction of two big bool vectors and return pointer to a big bool vector
-bb* bb_xor (bb* vector1, bb* vector2); //makes a xor of two big bool vectors and return pointer to a big bool vector
-bb* bb_inversion (bb* vector1); //makes a inversion of big bool vector and return pointer to a big bool vector
+/*
+ * Funtion: rigth_shift
+ * --------------------
+ * Do rigth shift of bool vector
+ *
+ *        vector: pointer to a vector
+ * size_of_shift: the number by which the rigth shift will be made
+ *
+ * returns: a pointer to a vector in case of success
+ *          NULL in case of an error
+ *
+ * if an error occurs the bb_errno variable is set to:
+ *     ERR_MEM_NOT_ALLOC — memory allocation error
+ *     ERR_NULL_INPUT    — zero incoming data
+ *     ERR_BIG_SHIFT     — too big a shift, going over the border
+ *     ERR_SMALL_SHIFT   — shift less than 1
+ */
+bb* rigth_shift (bb* vector, int size_of_shift);
+
+/*
+ * Funtion: cycle_left_shift
+ * -------------------------
+ * Do cyclic left shift of bool vector
+ *
+ *        vector: pointer to a vector
+ * size_of_shift: the number by which the cyclic left shift will be made
+ *
+ * returns: a pointer to a vector in case of success
+ *          NULL in case of an error
+ *
+ * if an error occurs the bb_errno variable is set to:
+ *     ERR_NULL_INPUT    — zero incoming data
+ *     ERR_SMALL_SHIFT   — shift less than 1
+ */
+bb* cycle_left_shift (bb* vector, int size_of_shift);
+
+/*
+ * Funtion: cycle_rigth_shift
+ * --------------------------
+ * Do cyclic rigth shift of bool vector
+ *
+ *        vector: pointer to a vector
+ * size_of_shift: the number by which the cyclic rigth shift will be made
+ *
+ * returns: a pointer to a vector in case of success
+ *          NULL in case of an error
+ *
+ * if an error occurs the bb_errno variable is set to:
+ *     ERR_NULL_INPUT    — zero incoming data
+ *     ERR_SMALL_SHIFT   — shift less than 1
+ */
+bb* cycle_rigth_shift (bb* vector, int size_of_shift);
+
+/* -------------------------------------------------------------------------
+ * Transformations
+ * ------------------------------------------------------------------------- */
+
+ /*
+  * Funtion: bb_from_string
+  * -----------------------
+  * Makes a boolean vector from a string containing 0 and 1
+  *
+  * string: pointer to a string
+  *
+  * returns: a pointer to a vector in case of success
+  *          NULL in case of an error
+  *
+  * if an error occurs the bb_errno variable is set to:
+  *     ERR_MEM_NOT_ALLOC — memory allocation error
+  *     ERR_NULL_INPUT    — zero incoming data
+  *     ERR_WRONG_CHARS   — extraneous characters were encountered in the string
+  */
+bb* bb_from_string (char* string);
+
+/*
+ * Funtion: bb_from_string
+ * -----------------------
+ * Makes a string of 1 and 0 from a Boolean vector
+ *
+ * vector: pointer to a vector
+ *
+ * returns: pointer to a string in case of success
+ *          NULL in case of an error
+ *
+ * if an error occurs the bb_errno variable is set to:
+ *     ERR_MEM_NOT_ALLOC — memory allocation error
+ *     ERR_NULL_INPUT    — zero incoming data
+ */
+char* bb_to_string (bb* vector);
+
+/* -------------------------------------------------------------------------
+ * Logical operations
+ * ------------------------------------------------------------------------- */
+
+ /*
+  * Funtion: bb_disjunction
+  * -----------------------
+  * Makes a disjunction of two boolean vectors
+  *
+  * vector1: pointer to a first vector
+  * vector2: pointer to a second vector
+  *
+  * returns: a pointer to a vector in case of success
+  *          NULL in case of an error
+  *
+  * if an error occurs the bb_errno variable is set to:
+  *     ERR_MEM_NOT_ALLOC — memory allocation error
+  *     ERR_NULL_INPUT    — zero incoming data
+  */
+bb* bb_disjunction (bb* vector1, bb* vector2);
+
+/*
+ * Funtion: bb_conjunction
+ * -----------------------
+ * Makes a conjunction of two boolean vectors
+ *
+ * vector1: pointer to a first vector
+ * vector2: pointer to a second vector
+ *
+ * returns: a pointer to a vector in case of success
+ *          NULL in case of an error
+ *
+ * if an error occurs the bb_errno variable is set to:
+ *     ERR_MEM_NOT_ALLOC — memory allocation error
+ *     ERR_NULL_INPUT    — zero incoming data
+ */
+bb* bb_conjunction (bb* vector1, bb* vector2);
+
+/*
+ * Funtion: bb_xor
+ * ---------------
+ * Makes a xor of two boolean vectors
+ *
+ * vector1: pointer to a first vector
+ * vector2: pointer to a second vector
+ *
+ * returns: a pointer to a vector in case of success
+ *          NULL in case of an error
+ *
+ * if an error occurs the bb_errno variable is set to:
+ *     ERR_MEM_NOT_ALLOC — memory allocation error
+ *     ERR_NULL_INPUT    — zero incoming data
+ */
+bb* bb_xor (bb* vector1, bb* vector2);
+
+/*
+ * Funtion: bb_inversion
+ * ---------------------
+ * Makes a inversion of bool vector
+ *
+ * vector1: pointer to a vector
+ *
+ * returns: a pointer to a vector in case of success
+ *          NULL in case of an error
+ *
+ * if an error occurs the bb_errno variable is set to:
+ *     ERR_MEM_NOT_ALLOC — memory allocation error
+ *     ERR_NULL_INPUT    — zero incoming data
+ */
+bb* bb_inversion (bb* vector1);
 
 #endif // BB_H
